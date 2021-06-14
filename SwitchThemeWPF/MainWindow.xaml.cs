8d8c792f371cdfe8d29a295f607a526213bf9fb4
AnSt <starininandrey@gmail.com>
2021-06-14 14:53:50 +0300

    Inint

---
+++ SwitchThemeWPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SwitchThemeWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+ 
+            var styles = new List<string> { "light", "dark" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "dark";
+        }
+ 
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            var style = styleBox.SelectedItem as string;
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+            var resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
+}
